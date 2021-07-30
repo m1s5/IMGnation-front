04249baba23e89bc54055760c67c3a3517d29147
Nikita <nikita.grechka@bk.ru>
2021-07-30 13:56:12 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          продам гараж
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
